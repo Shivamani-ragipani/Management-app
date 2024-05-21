3c5d38ae31bff044e3fa118b465974f2a982dbdb
Shivamani-ragipani <shivamaniragipani@gmail.com>
2024-05-21 19:00:13 +0530

    Components - ProjectSideBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import ProjectSideBar from "./Components/ProjectSideBar.jsx";
+
 function App() {
   return (
-    <>
-      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
-    </>
+    <main>
+      <ProjectSideBar />
+    </main>
   );
 }
 
---
+++ src/Components/ProjectSideBar.jsx
@@ -0,0 +1,13 @@
+export default function ProjectSideBar(){
+    return (
+        <aside>
+            <h2>New Project</h2>
+            <button>
+                + Add Project
+            </button>
+            <ul>
+                
+            </ul>
+        </aside>
+    );
+}
